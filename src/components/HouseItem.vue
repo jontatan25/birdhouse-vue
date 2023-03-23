522941fdad695dc3e31e9f47f535ae6dbf9b36d3
Jonathan Sarmiento <jhonn.photo@gmail.com>
2023-03-23 04:14:36 -0500

    feat/HouseItem Updated. Link implemented and hover styles were added.

--- src/components/HouseItem.vue
+++ src/components/HouseItem.vue
@@ -1,27 +1,29 @@
 <template>
-  <div class="w-60 h-40 px-6 py-7 bg-black-russian rounded-xl">
-    <h3 class="font-poppins font-semibold text-2xl text-white mb-5">
-      Cool birdhouse
-    </h3>
-    <div class="flex justify-between mb-2">
-      <img src="../assets/img/location.svg" alt="location" />
-      <span class="text-white font-poppins font-normal"
-        >({{ birdhouse.longitude }},{{ birdhouse.latitude }})</span
-      >
-    </div>
-    <div class="flex">
-      <div class="flex justify-between w-10 mr-3">
-        <img src="../assets/img/pets.svg" alt="location" />
-        <span class="text-white font-poppins font-normal">{{
-          birdhouse.birds
-        }}</span>
+  <router-link :to="'/dashboard/house/' + birdhouse.id" class="house-item-link">
+    <div class="house-item w-60 h-40 px-6 py-7 bg-black-russian rounded-xl">
+      <h3 class="font-poppins font-semibold text-2xl text-white mb-5">
+        Cool birdhouse
+      </h3>
+      <div class="flex justify-between mb-2">
+        <img src="../assets/img/location.svg" alt="location" />
+        <span class="text-white font-poppins font-normal"
+          >({{ birdhouse.longitude }},{{ birdhouse.latitude }})</span
+        >
       </div>
-      <div class="flex justify-between w-9">
-        <img src="../assets/img/eggs.svg" alt="location" />
-        <span class="text-white font-poppins">{{ birdhouse.eggs }}</span>
+      <div class="flex">
+        <div class="flex justify-between w-10 mr-3">
+          <img src="../assets/img/pets.svg" alt="location" />
+          <span class="text-white font-poppins font-normal">{{
+            birdhouse.birds
+          }}</span>
+        </div>
+        <div class="flex justify-between w-9">
+          <img src="../assets/img/eggs.svg" alt="location" />
+          <span class="text-white font-poppins">{{ birdhouse.eggs }}</span>
+        </div>
       </div>
     </div>
-  </div>
+  </router-link>
 </template>
 
 <script lang="ts">
@@ -38,3 +40,14 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.house-item {
+  transition: all 0.5s ease-in-out;
+}
+.house-item:hover {
+  transform: scale(1.05);
+  transition: all 0.2s ease-in-out;
+  outline: 1px solid rgb(75, 85, 99);
+}
+</style>
